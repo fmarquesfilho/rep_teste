3854bfe628da0d6c07577b438f407d1c7170dfbb
Fernando Figueira <690686+fmarquesfilho@users.noreply.github.com>
2025-09-03 17:53:01 -0300

    adiciona outra identação

--- eq_2grau.c
+++ eq_2grau.c
@@ -10,15 +10,17 @@ int main() {
     delta = (b * b) - (4 * a * c); 
 
     if (delta < 0) {
-        printf("Não existem raízes reais\n");
-    } else if (delta == 0) {
-        r1 = (-b + sqrt(delta)) / (2 * a);
-        printf("Esta equação só tem uma raíz real: %.2f\n", r1);
-    } else {
-        /* caso em que delta > 0 */
-        r1 = (-b + sqrt(delta)) / (2 * a);
-        r2 = (-b - sqrt(delta)) / (2 * a);
-        printf("Esta equação tem duas raízes reais: %.2f %.2f\n", r1, r2);
+        printf("Não existem raízes reais\n"); printf("Oi mundo");
+    } else { 
+        if (delta == 0) {
+            r1 = (-b + sqrt(delta)) / (2 * a);
+            printf("Esta equação só tem uma raíz real: %.2f\n", r1);
+        } else {
+            /* caso em que delta > 0 */
+            r1 = (-b + sqrt(delta)) / (2 * a);
+            r2 = (-b - sqrt(delta)) / (2 * a);
+            printf("Esta equação tem duas raízes reais: %.2f %.2f\n", r1, r2);
+        }
     }
 
     return 0;
