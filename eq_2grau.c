a076e64aa55238633a502a1120827d6cc44d84bf
Fernando Figueira <690686+fmarquesfilho@users.noreply.github.com>
2025-09-03 17:40:49 -0300

    adiciona última versão ex eq 2 grau

--- eq_2grau.c
+++ eq_2grau.c
@@ -2,27 +2,23 @@
 #include<math.h>
 
 int main() {
-    int a, b, c;
-    float delta, r1, r2;
+    float a, b, c, delta, r1, r2;
 
     printf("Digite a b c: \n");
-    scanf("%d %d %d", &a, &b, &c);
+    scanf("%f %f %f", &a, &b, &c);
 
     delta = (b * b) - (4 * a * c); 
 
     if (delta < 0) {
         printf("Não existem raízes reais\n");
-    }
-
-    if (delta == 0) {
+    } else if (delta == 0) {
         r1 = (-b + sqrt(delta)) / (2 * a);
-        printf("Esta equação só tem uma raíz real: %f\n", r1);
-    }
-
-    if (delta > 0) {
+        printf("Esta equação só tem uma raíz real: %.2f\n", r1);
+    } else {
+        /* caso em que delta > 0 */
         r1 = (-b + sqrt(delta)) / (2 * a);
         r2 = (-b - sqrt(delta)) / (2 * a);
-        printf("Esta equação tem duas raízes reais: %f %f\n", r1, r2);
+        printf("Esta equação tem duas raízes reais: %.2f %.2f\n", r1, r2);
     }
 
     return 0;
