65c3076f78e58c1ff8856dfa688f7d00faa5edb5
Fernando Figueira <690686+fmarquesfilho@users.noreply.github.com>
2025-09-03 17:33:17 -0300

    altera equação do 2 grau para incluir if's

--- eq_2grau.c
+++ eq_2grau.c
@@ -10,11 +10,20 @@ int main() {
 
     delta = (b * b) - (4 * a * c); 
 
-    r1 = (-b + sqrt(delta)) / (2 * a);
-    r2 = (-b - sqrt(delta)) / (2 * a);
+    if (delta < 0) {
+        printf("Não existem raízes reais\n");
+    }
 
-    printf("Raíz 1: %f\n", r1);
-    printf("Raíz 2: %f\n", r2);
+    if (delta == 0) {
+        r1 = (-b + sqrt(delta)) / (2 * a);
+        printf("Esta equação só tem uma raíz real: %f\n", r1);
+    }
+
+    if (delta > 0) {
+        r1 = (-b + sqrt(delta)) / (2 * a);
+        r2 = (-b - sqrt(delta)) / (2 * a);
+        printf("Esta equação tem duas raízes reais: %f %f\n", r1, r2);
+    }
 
     return 0;
 }
